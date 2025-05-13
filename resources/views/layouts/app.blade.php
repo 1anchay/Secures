c69747317d492034dd6c63f0c24be8ae472706d8
ivan zhivo <vtar4a@gmail.com>
2025-05-13 23:33:01 +0300

    asd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,13 +8,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet"> <!-- Подключаем стили -->
+    <link href="{{ vite('css/app.css') }}" rel="stylesheet"> <!-- Подключаем стили -->
 </head>
 @stack('scripts')
 
 <body class="bg-gray-100">
     @yield('content') <!-- Содержимое страницы будет вставляться сюда -->
 
-    <script src="{{ mix('js/app.js') }}"></script> <!-- Подключаем скрипты -->
+    <script src="{{ vite('js/app.js') }}"></script> <!-- Подключаем скрипты -->
 </body>
 </html>

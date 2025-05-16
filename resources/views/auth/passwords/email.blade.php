ce8967162300938f513257f0917e27c0abe5c4f4
ivan zhivo <vtar4a@gmail.com>
2025-05-16 15:06:08 +0300

    12313qweqw4eq

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,3 +1,7 @@
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://unpkg.com/heroicons@1.0.6/outline/index.js"></script>
 @extends('layouts.app')
 
 @include('header')
